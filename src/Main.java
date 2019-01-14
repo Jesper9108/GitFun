a9aa99caf13432238afee6020255fc65587a1a90
Peter Kovacs <pitner33@gmail.com>
2019-01-14 13:53:50 +0100

    d1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+
+    }
     Main(){
         System.out.println("ASd");
+    }
+
+    public String future() {
+        return"Virgo";
     }
 }
