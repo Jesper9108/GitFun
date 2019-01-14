aaa1ca3f914dd0cb449c74d907e5cf185f2e17d8
Peter Kovacs <pitner33@gmail.com>
2019-01-14 14:04:06 +0100

    d1.3

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,14 @@ public class Main {
         System.out.println("ASd");
     }
 
-<<<<<<< HEAD
+
     public void future() {
         System.out.println("Virgo");
-=======
-    public void cheer() {
-        System.out.println("Rajcsapat");
->>>>>>> 5ef4fb3d0fc8dd75b15286aa8d2be27a16373286
     }
+
+    public void cheer () {
+        System.out.println("Rajcsapat");
+
+    }
+
 }
