ebdf5e214a547302e803233ec6d37d17e6f3ae81
refike <daniel.megyaszai@gmail.com>
2019-01-14 14:08:51 +0100

    b1

---
+++ src/Daniclass.java
@@ -0,0 +1,8 @@
+public class Daniclass {
+
+    public void Count() {
+        int age = 26;
+        System.out.println("My age is:" + age);
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,6 @@ public class Main {
     Main(){
         System.out.println("ASd");
     }
+
+
 }
