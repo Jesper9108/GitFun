c0fe541e4a4fa0ff69337c826e583fb4f5e1c87b
Zsolt Jenofi <jenofi.zsolt@gmail.com>
2019-01-14 15:07:09 +0100

    random

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("ASd");
     }
 
-    public void greet(String input, String input2) {
+    public void greet1(String input, String input2) {
         System.out.println(input + input2);
     }
     
