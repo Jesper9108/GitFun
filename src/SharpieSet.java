9d0c0ce79665ea7b92c31aa83dbc804bf6f19511
Peter Kovacs <pitner33@gmail.com>
2019-01-14 15:18:07 +0100

    d6

--- src/SharpieSet.java
+++ src/SharpieSet.java
@@ -4,6 +4,6 @@ public class SharpieSet {
 
     public void cheer(String idemondjad) {
         idemondjad = "english";
-        System.out.println(idemondjad);
+        System.out.println("nemmondom");
     }
 }
